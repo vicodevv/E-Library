8a0fee7e62bb3e338ebc158532598e1a8fe34dd0
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-03-21 14:59:57 +0100

    Initialized Backend app

---
+++ Backend/src/main/java/com/example/library/Library.java
@@ -0,0 +1,56 @@
+package com.example.library;
+
+public class Library {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long phoneNumber;
+    private String address;
+
+    public Library(){
+
+    }
+    public Library(String firstName, String lastName, String email, Long phoneNumber, String address){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
