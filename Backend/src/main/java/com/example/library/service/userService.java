eb7035722308cb1169f913dcf2541540e671c8e1
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-03-28 14:40:06 +0100

    Added Book and User Repository

--- Backend/src/main/java/com/example/library/service/userService.java
+++ Backend/src/main/java/com/example/library/service/userService.java
@@ -2,6 +2,7 @@ package com.example.library.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.library.User;
@@ -10,7 +11,8 @@ import com.example.library.Repository.UserRepository;
 @Service
 public class userService {
     private final UserRepository userRepository;
-
+    
+    @Autowired
     public userService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
