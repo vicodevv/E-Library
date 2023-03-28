c78648909cd3082775cd790631cdaac6e5972b29
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-03-28 10:18:42 +0100

    Book Class

--- Backend/src/main/java/com/example/library/Book.java
+++ Backend/src/main/java/com/example/library/Book.java
@@ -1,5 +1,26 @@
 package com.example.library;
 
 public class Book {
-    
+    private String title;
+    private String author;
+    private String publishedDate;
+    private Enum<E> Category;
+    private Enum<E> packageType;
+    private Number availableQuantity;
+    private Number totalQuantity;
+    private String coverPage;
+
+    public Book(){   
+    }
+
+    public Book(String title, String author, String publishedDate, Enum<E> Category, Enum<E> packageType, Number availableQuantity, Number totalQuantity, String coverPage){
+        this.title = title;
+        this.author = author;
+        this.publishedDate = publishedDate;
+        this.Category = Category;
+        this.packageType = packageType;
+        this.availableQuantity = availableQuantity;
+        this.totalQuantity = totalQuantity;
+        this.coverPage = coverPage;
+    }
 }
