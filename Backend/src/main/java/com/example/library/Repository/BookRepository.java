4feaf0d7bcc77c29afb97f23139c4923c7bfc4df
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-03-28 12:57:15 +0100

    Added Book and User Repository

--- Backend/src/main/java/com/example/library/Repository/BookRepository.java
+++ Backend/src/main/java/com/example/library/Repository/BookRepository.java
@@ -1,8 +1,10 @@
 package com.example.library.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.library.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    
+    Optional<Book> finduserByTitle(String title);
 }
--- Backend/src/main/java/com/example/library/Repository/userRepository.java
+++ Backend/src/main/java/com/example/library/Repository/userRepository.java
@@ -1,5 +1,7 @@
 package com.example.library.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,6 @@ import com.example.library.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> finduserByEmail(String email);
 }
 
