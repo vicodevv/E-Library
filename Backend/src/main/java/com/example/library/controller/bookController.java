0477f35acb72a352858ca23d4f885063e41c024f
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-03-28 11:22:24 +0100

    Added Book and User Repository

--- Backend/src/main/java/com/example/library/controller/bookController.java
+++ Backend/src/main/java/com/example/library/controller/bookController.java
@@ -1,5 +1,15 @@
 package com.example.library.controller;
 
+import java.util.List;
+
+import com.example.library.Book;
+
 public class bookController {
+
+    public List<Book> getBooks() {
+        return List.of(
+            new Book("Harry Potter", "J.K. Rowling", "1234567890", "Fantasy", "English", "123 Main St")
+        );
+    }
     
 }
