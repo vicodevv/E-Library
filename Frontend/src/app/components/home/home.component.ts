0da904061b43d9c6e5a79f85ee836dbc49914255
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-03-30 16:38:05 +0100

    Home component

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { APIResponse, Book } from 'src/app/models';
 import { HttpService } from 'src/app/service/http.service';
 @Component({
@@ -7,14 +9,15 @@ import { HttpService } from 'src/app/service/http.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent{
-  public books!: Array<Book>;
+export class HomeComponent implements OnInit{
+  books: any;
+  
+  constructor( private http: HttpClient) { }
 
-  constructor(
-    private httpService: HttpService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-  ) { }
 
+  ngOnInit(): void {
+    let response = this.http.get<APIResponse<Book>>(`http://localhost:8080/api/books`);
+    response.subscribe((data) => this.books = data);
+}
 
 }
