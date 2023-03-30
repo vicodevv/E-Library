b164f339cb97b831fe7d820edd2222ae4cd8f165
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-03-30 11:13:15 +0100

    Http Service

---
+++ Frontend/src/app/service/http.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { forkJoin, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment as env } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+  constructor() { }
+}
