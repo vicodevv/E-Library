2e87f1ec28b86bc06840304a0c2986eeb6e9e8d8
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-03-30 11:40:54 +0100

    Http Service

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { APIResponse, Book } from 'src/app/models';
+import { HttpService } from 'src/app/service/http.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public books!: Array<Book>;
+
+  constructor(
+    private httpService: HttpService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.httpService.getBooks().subscribe((response: APIResponse<Book>) => {
+      this.books = response.results;
+    });
+  }
 
 }
