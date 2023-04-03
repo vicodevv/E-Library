6945f26ef4cb96c3575557c66638fc9f09ed2d04
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-03 17:33:58 +0100

    Modal container setup

--- Frontend/src/app/components/modal/modal.component.ts
+++ Frontend/src/app/components/modal/modal.component.ts
@@ -4,12 +4,10 @@ import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent {
-  constructor(public modalRef: MdbModalRef<ModalComponent>) {}
 
-  close(): void {
-    const closeMessage = 'Modal closed';
-    this.modalRef.close(closeMessage)
+  constructor(public modalRef: MdbModalRef<ModalComponent>) { }
+  
   }
-}
