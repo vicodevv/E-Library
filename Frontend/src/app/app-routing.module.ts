3802abbd441c048dd787298921bedaf530c80c59
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-03 13:18:43 +0100

    Routeer fix

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { BooksComponent } from './components/books/books.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'books', component: BooksComponent },
+  //{ path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
