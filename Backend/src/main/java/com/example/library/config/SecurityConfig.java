85fa605855c98c79074a09ccffb74d9a8fc104d5
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-04 15:56:01 +0100

    Security Cinfiguration

--- Backend/src/main/java/com/example/library/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/library/config/SecurityConfig.java
@@ -6,12 +6,17 @@ import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @Configuration
-public class SecurityConfig {
+@EnableWebMvc
+public class SecurityConfig{
 
     @Bean
     @Primary
@@ -24,6 +29,19 @@ public class SecurityConfig {
         config.setAllowedMethods(Collections.singletonList("*"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors().and()
+            .csrf().disable()
+            .authorizeRequests()
+            .requestMatchers("/api/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+        return http.build();
     }
     
 }
