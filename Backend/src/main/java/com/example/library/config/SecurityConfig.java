1e6747063f5e76ff4bef3bf9aa0f5826bf02769c
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-08 12:57:08 +0100

    Lower Java version

--- Backend/src/main/java/com/example/library/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/library/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
@@ -34,7 +35,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        super.configure(http);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(null);
     }
       
     @Bean
---
+++ Backend/src/main/java/com/example/library/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,18 @@
+package com.example.library.filter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        // TODO Auto-generated method stub
+        return super.attemptAuthentication(request, response);
+    }
+    
+}
