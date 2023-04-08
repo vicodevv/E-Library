3d99b46c38cc850f8de135fa83b2d8ae8b34b266
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-08 13:03:34 +0100

    Lower Java version

--- Backend/src/main/java/com/example/library/filter/CustomAuthenticationFilter.java
+++ Backend/src/main/java/com/example/library/filter/CustomAuthenticationFilter.java
@@ -1,18 +1,42 @@
 package com.example.library.filter;
 
+import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+    private final AuthenticationManager authenticationManager;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        log.info("username is: {} " + username);
+        log.info("password is: {} " + password);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws java.io.IOException, javax.servlet.ServletException {
         // TODO Auto-generated method stub
-        return super.attemptAuthentication(request, response);
+        super.successfulAuthentication(request, response, chain, authResult);
     }
     
 }
