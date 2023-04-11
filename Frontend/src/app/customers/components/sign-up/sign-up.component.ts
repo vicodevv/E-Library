78143f1dd14dd171aa23b0769661f630042e8e8c
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-11 18:00:58 +0100

    Updated Customer Sign In and Sign Up  page

--- Frontend/src/app/customers/components/sign-up/sign-up.component.ts
+++ Frontend/src/app/customers/components/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpService } from 'src/app/service/http.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,7 +9,9 @@ import { Router } from '@angular/router';
 })
 export class SignUpComponent {
   
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private httpService: HttpService,) {}
 
   homePage(): void {
     this.router.navigateByUrl('');
@@ -16,6 +19,12 @@ export class SignUpComponent {
 
   signInPage(): void {
     this.router.navigateByUrl('sign-in');
+  }
+
+  addNewUser() {
+    this.httpService.addNewUser({}).subscribe((response: any) => {
+      console.log(response);
+    });
   }
 
 
