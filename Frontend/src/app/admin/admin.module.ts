8ea634fc978ec30673d8e4c22db580957bc120ab
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-11 10:09:48 +0100

    Fixed User Details Service

--- Frontend/src/app/admin/admin.module.ts
+++ Frontend/src/app/admin/admin.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AdminRoutingModule } from './admin-routing.module';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { AdminComponent } from './admin.component';
+import { BooksComponent } from './components/books/books.component';
 
 
 @NgModule({
   declarations: [
+    AdminComponent,
     SignInComponent,
     SignUpComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    BooksComponent
   ],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    CommonModule,
+    AdminRoutingModule
+  ],
+  bootstrap: [AdminComponent]
 })
 export class AdminModule { }
--- Frontend/src/app/admin/components/header/header.component.ts
+++ Frontend/src/app/admin/components/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  constructor(private router: Router) {}
+
+  booksPage(): void {
+    this.router.navigateByUrl('admin/books');
+  }
 
 }
