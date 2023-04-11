d72cc3af6daff79e57741a8be2b60f9d74cab6cc
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-11 10:51:41 +0100

    Custom Authorization Filter

--- Backend/src/main/java/com/example/library/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/library/config/SecurityConfig.java
@@ -38,17 +38,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests()
-            .antMatchers(HttpMethod.POST,"/login").permitAll()
+            .antMatchers(HttpMethod.POST,"/api/login").permitAll()
             .antMatchers(HttpMethod.POST, "/api/users/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/role/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/books/**").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/books/**").permitAll();
-        //http.authorizeRequests().anyRequest().authenticated();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/books/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
     }
       
     @Bean
