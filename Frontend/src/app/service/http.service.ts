942d6f60d38ffd355c9f030b8104988575beca5e
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-12 14:16:06 +0100

    Fixed errors

--- Frontend/src/app/service/http.service.ts
+++ Frontend/src/app/service/http.service.ts
@@ -5,14 +5,16 @@ import { APIResponse, Book } from '../models';
 import { Observable, throwError, BehaviorSubject } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
   private readonly BASE_URL = env.BASE_URL; 
+  loginStatus = new BehaviorSubject<boolean>(this.hasToken());
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,  private cookieService: CookieService) { }
 
   public getAllBooks(){
     return this.http.get<APIResponse<Book>>(`${this.BASE_URL}/api/books`);
@@ -20,10 +22,29 @@ export class HttpService {
   public addNewUser(user: any){
     return this.http.post(`${this.BASE_URL}/api/users`, user);
   }
+
+  
+
+  // Login a user
+  public login(formData:any):Observable<HttpResponse<any>>{
+    return this.http.post<any>(this.BASE_URL+"login",formData,  { observe: 'response' })
+    .pipe(
+      tap((resp: HttpResponse<any>) => {
+        if(resp.body.token){
+          //this.cookieService.set("currentUser",resp.headers.get('x-auth'));
+          this.loginStatus.next(true);
+        }
+        return resp;  
+      }),
+      catchError(this.handleError)
+    );
+  }
     /**
    * 
    * @param formData as the login form data
    */
+
+  // Sign up a new user
   public signUp(formData:any):Observable<HttpResponse<any>>{
       return this.http.post<any>(this.BASE_URL+"api/users",formData,  { observe: 'response' })
       .pipe(
@@ -34,6 +55,7 @@ export class HttpService {
       );
     }
     
+    // Handle Http operation that failed.
     private handleError(error: HttpErrorResponse) {
       if (error.error instanceof ErrorEvent) {
         // A client-side or network error occurred. Handle it accordingly.
@@ -49,4 +71,9 @@ export class HttpService {
       return throwError(
         'Something bad happened; please try again later.');
     };
+
+    // Check if the user has a token
+    private hasToken() : boolean {
+      return this.cookieService.check('currentUser');
+    }
 }
