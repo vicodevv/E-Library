1a225f9fa344ab43a01970d5320affebfc1b0abc
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-14 13:21:30 +0100

    Refactored Code

---
+++ Backend/src/main/java/com/example/library/filter/JWTAuthorizationFilter.java
@@ -0,0 +1,40 @@
+package com.example.library.filter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.library.manager.SecurityConstants;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String header = request.getHeader(SecurityConstants.AUTHORIZATION);
+
+        if (header == null || !header.startsWith(SecurityConstants.BEARER)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.replace(SecurityConstants.BEARER, "");
+        String user = JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET_KEY))
+                .build()
+                .verify(token)
+                .getSubject();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, Arrays.asList());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+}
