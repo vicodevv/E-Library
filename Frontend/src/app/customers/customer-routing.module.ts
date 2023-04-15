6de7543aee521279c1b8f44ba068abd6e40ceaf8
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-15 10:09:39 +0100

    Fixed Logout issue

--- Frontend/src/app/customers/customer-routing.module.ts
+++ Frontend/src/app/customers/customer-routing.module.ts
@@ -4,18 +4,12 @@ import { HomeComponent } from './components/home/home.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { BooksComponent } from './components/books/books.component';
-import { LoginGuard } from './components/sign-in/sign-in.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'books', component: BooksComponent },
-  {
-    path: '',
-    component: SignInComponent,
-    canActivate: [LoginGuard],
-}
   //{ path: '**', component: PageNotFoundComponent }
 ];
 
