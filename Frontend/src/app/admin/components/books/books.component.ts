48254afadce1064091a2b064bc7e07a36141b92d
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-20 11:43:57 +0100

    Admin Post BoOKS

--- Frontend/src/app/admin/components/books/books.component.ts
+++ Frontend/src/app/admin/components/books/books.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from 'express';
+import { HttpService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit{
 
+  booksForm: any = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    author: new FormControl('', [Validators.required]),
+    publishedDate: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+    packageType: new FormControl('', [Validators.required]),
+    availableQuantity: new FormControl('', [Validators.required]),
+    totalQuantity: new FormControl('', [Validators.required]),
+    coverPage: new FormControl('', [Validators.required]),
+  });
+
+  constructor(
+    private httpService : HttpService,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(): void {
+    const { title, author, publishedDate, category, packageType, availableQuantity, totalQuantity, coverPage } = this.booksForm;
+
+    this.httpService.addBook(title, author, publishedDate, category, packageType, availableQuantity, totalQuantity, coverPage).subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
 }
